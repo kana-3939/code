cb4d242832ee7b5e138465548b4ee847deb4a7fe
Owner <Owner@h70221>
2025-11-27 18:20:56 +0900

    Java2.3

---
+++ HelloWorld_KanaiYu/src/sample/SelfIntroduction.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main (String[] args){
+
+		
+  String name = "金井　優";
+  int age = 21;
+  String hobby = "カフェめぐり";
+  
+  System.out.println("私の名前は" + name +"です。");
+  System.out.println("年齢は" + age + "です。");
+  System.out.println("趣味は" + hobby+ "です。");
+
+	}
+}
