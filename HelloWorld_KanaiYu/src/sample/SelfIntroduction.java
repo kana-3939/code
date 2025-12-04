47f8db6fdf835e499a3ffc23e712862080a428be
Owner <Owner@h70221>
2025-12-04 12:30:29 +0900

    課題1～13

--- HelloWorld_KanaiYu/src/sample/SelfIntroduction.java
+++ HelloWorld_KanaiYu/src/sample/SelfIntroduction.java
@@ -4,13 +4,24 @@ public class SelfIntroduction {
 
 	public static void main (String[] args){
 
-		
+
+//氏名
   String name = "金井　優";
+  
+  
+//年齢
   int age = 21;
+
+//趣味
   String hobby = "カフェめぐり";
   
+  
+/*
+ * 自己紹介文を出力する処理
+ */
+ 
   System.out.println("私の名前は" + name +"です。");
-  System.out.println("年齢は" + age + "です。");
+  System.out.println("年齢は" + age + "歳です。");
   System.out.println("趣味は" + hobby+ "です。");
 
 	}
